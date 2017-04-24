69b02c6f19425fe8578c0e7b481dddb60f7d595c
kenkoji <kenkoji313@gmail.com>
2017-04-24 02:16:00 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,20 +2,21 @@ console.log("Up and running!");
 
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push("cardOne");
-var cardTwo = cards[2];
-cardsInPlay.push("cardTwo")
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-	
-	if (cardsInPlay.length === 2)
-	if (cardsInPlay[0] === cardsInPlay[1]) {	
+function checkForMatch() {if (cardsInPlay[0] === cardsInPlay[1]) {	
 		alert("You found a match!")
 	} else {
 		alert("Sorry, try again.");
 	}
-console.log("User flipped " + cardThree);
 
+}
+
+
+function flipCard(cardId) {
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+}
+flipCard(0);
+flipCard(2);
+	
+	
