81da260b878be726bc46d104972ed496e0823614
kenkoji <kenkoji313@gmail.com>
2017-04-24 01:28:45 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,6 @@
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
 var cardOne = cards[0];
 cardsInPlay.push("cardOne");
@@ -11,8 +11,9 @@ console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
 
 	
-	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]); {	
-		alert("You found a match!");
+	if (cardsInPlay.length === 2)
+	if (cardsInPlay[0] === cardsInPlay[1]) {	
+		alert("You found a match!")
 	} else {
 		alert("Sorry, try again.");
 	}
